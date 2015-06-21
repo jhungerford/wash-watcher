3a0351129970f2c7e09feee96fbd82cc47e67f67
John Hungerford <jhungerford@gmail.com>
2015-06-21 09:04:08 -0600

    Display raw accelerometer data.

--- app/src/main/java/dev/jhungerford/washwatcher/MyActivity.java
+++ app/src/main/java/dev/jhungerford/washwatcher/MyActivity.java
@@ -1,22 +1,43 @@
 package dev.jhungerford.washwatcher;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
 
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements SensorEventListener {
 
     public static final String EXTRA_MESSAGE = "dev.jhungerford.washwatcher.MESSAGE";
+
+    private TextView xTextView;
+    private TextView yTextView;
+    private TextView zTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        xTextView = (TextView) findViewById(R.id.acc_x);
+        yTextView = (TextView) findViewById(R.id.acc_y);
+        zTextView = (TextView) findViewById(R.id.acc_z);
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -41,15 +62,15 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xTextView.setText(Float.toString(event.values[0]));
+        yTextView.setText(Float.toString(event.values[1]));
+        zTextView.setText(Float.toString(event.values[2]));
+    }
 
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-        intent.putExtra(EXTRA_MESSAGE, message);
-
-        startActivity(intent);
     }
 }
