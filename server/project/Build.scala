25c152929df60ef22d5f102c6e8a09c669546cef
John Hungerford <jhungerford@gmail.com>
2015-07-03 08:14:58 -0600

    Multi-module project.
    http://xerial.org/blog/2014/03/26/buidling-multi-module-projects-in-sbt/

--- server/project/Build.scala
+++ server/project/Build.scala
@@ -2,14 +2,37 @@ import sbt._
 import sbt.Keys._
 
 object WashWatcherBuild extends Build {
+  lazy val scalatraVersion = "2.3.1"
 
-  scalaVersion := "2.10.4"
+  lazy val root = project.in(file(".")).aggregate(server, interface)
 
-  lazy val washWatcher = Project(
-    id = "WashWatcher",
-    base = file("."))
+
+  lazy val commonSettings = Seq(
+    organization := "dev",
+    version := "0.0.1",
+    scalaVersion := "2.10.5"
+  )
+
+  lazy val server = project
+    .in(file("washwatcher-server"))
+    .settings(commonSettings: _*)
     .settings(
-      name := "WashWatcher",
-      version := "0.0.1",
-      parallelExecution in Test := false)
+      name := "washwatcher-server",
+      libraryDependencies ++= Seq(
+        "org.scalatra" %% "scalatra" % scalatraVersion,
+        "org.scalatra" %% "scalatra-json" % scalatraVersion,
+        "org.json4s" %% "json4s-jackson" % "3.2.6",
+        "ch.qos.logback" % "logback-classic" % "1.1.3",
+        "org.eclipse.jetty" % "jetty-webapp" % "9.3.0.v20150612",
+        "org.scalatra" %% "scalatra-specs2" % scalatraVersion % "test",
+        "org.scalatest" %% "scalatest" % "2.2.1" % "test"
+      )
+    )
+
+  lazy val interface = project
+    .in(file("washwatcher-interface"))
+    .settings(commonSettings: _*)
+    .settings(
+      name := "washwatcher-interface"
+    )
 }
