05091b47c5e3af486ab755a5500a129f1e66efde
John Hungerford <jhungerford@gmail.com>
2015-07-06 07:13:23 -0600

    Finatra hello world.

---
+++ server/washwatcher-server/src/main/scala/dev/washwatcher/server/HelloWorldController.scala
@@ -0,0 +1,12 @@
+package dev.washwatcher.server
+
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.Controller
+
+class HelloWorldController extends Controller {
+
+  get("/api/v1/hi") { request: Request =>
+    info("hi")
+    "Hello " + request.params.getOrElse("name", "World") + "!"
+  }
+}
---
+++ server/washwatcher-server/src/main/scala/dev/washwatcher/server/WashWatcherServer.scala
@@ -0,0 +1,19 @@
+package dev.washwatcher.server
+
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.finatra.http.HttpServer
+import com.twitter.finatra.http.filters.CommonFilters
+import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.logging.filter.{TraceIdMDCFilter, LoggingMDCFilter}
+
+object WashWatcherServerMain extends WashWatcherServer
+
+class WashWatcherServer extends HttpServer {
+  override def configureHttp(router: HttpRouter) {
+    router
+      .filter[LoggingMDCFilter[Request, Response]]
+      .filter[TraceIdMDCFilter[Request, Response]]
+      .filter[CommonFilters]
+      .add[HelloWorldController]
+  }
+}
