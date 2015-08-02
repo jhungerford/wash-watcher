8b4e1fd40c0343e298695767d0ac5d4dc7ca145f
John Hungerford <jhungerford@gmail.com>
2015-08-02 11:53:38 -0600

    Line graph of sensor reading magnitudes.

--- server/washwatcher-server/src/main/resources/web/js/washwatcher.js
+++ server/washwatcher-server/src/main/resources/web/js/washwatcher.js
@@ -1,32 +1,31 @@
 $(function() {
-  var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        data: [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-        label: "My Second dataset",
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: [28, 48, 40, 19, 86, 27, 90]
-      }
-    ]
-  };
+  $.get("/api/v1/sensor/magnitudes")
+    .done(function(data) {
+      var labels = data.map(function(reading) {
+        return reading['when'];
+      });
 
-  var options = {};
+      var magnitudes = data.map(function(reading) {
+        return reading['magnitude'];
+      });
 
-  var ctx = document.getElementById("magnitudeChart").getContext("2d");
-  var chart = new Chart(ctx).Line(data, options);
+      var chartData = {
+        labels: labels,
+        datasets: [{
+          label: "Magnitudes",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: magnitudes
+        }]
+      };
+
+      var options = {};
+      var ctx = document.getElementById("magnitudeChart").getContext("2d");
+      var chart = new Chart(ctx);
+      chart.Line(chartData, options);
+    });
 });
