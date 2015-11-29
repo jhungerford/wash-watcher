16718e40a287cd2af50f1523754f84993ad8f748
John Hungerford <jhungerford@gmail.com>
2015-11-29 09:42:03 -0700

    Average magnitude resource.

--- phoenix_server/web/controllers/sensor_magnitude_controller.ex
+++ phoenix_server/web/controllers/sensor_magnitude_controller.ex
@@ -5,8 +5,16 @@ defmodule PhoenixServer.SensorMagnitudeController do
     query = from s in PhoenixServer.Sensor,
       where: s.timestamp > ^timestamp,
       select: s
+
     sensors = Repo.all(query)
 
-    json conn, %{num: length(sensors)}
+    num = length(sensors)
+    avg = List.foldl(sensors, 0, fn (sensor, acc) -> acc + magnitude(sensor) end) / num
+
+    json conn, %{num: num, avg: avg}
+  end
+
+  def magnitude(sensor) do
+    :math.sqrt(:math.pow(sensor.x, 2) + :math.pow(sensor.y, 2) + :math.pow(sensor.z, 2))
   end
 end
