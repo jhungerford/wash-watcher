795e9529511fa4acab69b8308a07578eae609688
John Hungerford <jhungerford@gmail.com>
2018-03-11 08:45:39 -0600

    LSM6DS3 Hello World - print id.

--- accel/accel.c
+++ accel/accel.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <errno.h>
+#include <wiringPiI2C.h>
 
 int main() {
-    printf("Hello, world\n");
+    printf("Running...\n");
+
+    int fd = wiringPiI2CSetup(I2C_ADDR);
+    if (fd < 0) {
+        printf("Error setting up i2c for address 0x%X - errorno %d\n", I2C_ADDR, errno);
+        return errno;
+    }
+
+    int who_are_you = wiringPiI2CReadReg8(fd, REG_WHO_AM_I);
+    printf("Who am I ID: 0x%X", who_are_you);
+
     return 0;
 }
---
+++ accel/accel.h
@@ -0,0 +1,3 @@
+#define I2C_ADDR 0x6B
+
+#define REG_WHO_AM_I 0x0F
