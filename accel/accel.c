64c64f090365371f482977367ee50ebbfed77edf
John Hungerford <jhungerford@gmail.com>
2018-03-13 08:17:26 -0600

    Print the accel registers.

--- accel/accel.c
+++ accel/accel.c
@@ -3,17 +3,40 @@
 #include <errno.h>
 #include <wiringPiI2C.h>
 
-int main() {
-    printf("Running...\n");
-
+/* 
+ * When power is applied to the LSM6DS3 module, it performs a 20ms boot
+ * procedure, then configures the accelerometer and gyroscope in power-down
+ * mode.  Init sets up wiring pi, enables the accelerometer, and returns
+ * the file descriptor for the i2c module.  If init returns a value less than 0,
+ * a fatal error occurred.
+ */
+int init() {
+    // Initialize wiring pi
     int fd = wiringPiI2CSetup(I2C_ADDR);
     if (fd < 0) {
         printf("Error setting up i2c for address 0x%X - errorno %d\n", I2C_ADDR, errno);
-        return errno;
+        return fd;
     }
 
-    int who_are_you = wiringPiI2CReadReg8(fd, REG_WHO_AM_I);
-    printf("Who am I ID: 0x%X\n", who_are_you);
+    // Enable the accelerometer
+    printf("CTRL1_XL: 0x%X\n", wiringPiI2CReadReg8(fd, REG_CTRL1_XL));
+    printf("CTRL6_C:  0x%X\n", wiringPiI2CReadReg8(fd, REG_CTRL6_C));
+
+    // Verify the Who am I register to ensure everything is working
+    int whoami = wiringPiI2CReadReg8(fd, REG_WHO_AM_I);
+    if (whoami != VALUE_WHO_AM_I) {
+        errno = EBADE;
+        return -1;
+    }
+
+    return fd;
+}
+
+int main() {
+    int fd = init();
+    if (fd < 0) {
+        return errno;
+    }
 
     return 0;
 }
--- accel/accel.h
+++ accel/accel.h
@@ -1,3 +1,10 @@
 #define I2C_ADDR 0x6B
 
 #define REG_WHO_AM_I 0x0F
+#define VALUE_WHO_AM_I 0x69
+
+#define REG_CTRL1_XL 0x10
+#define VALUE_CTRL1_XL_ODR_XL_104HZ 0x40
+
+#define REG_CTRL6_C 0x15
+#define VALUE_CTRL6_C_XL_HM_MODE_1 0x10
