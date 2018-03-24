4b9681d4dee92bea63dec8e764e9cdf392cb1e39
John Hungerford <jhungerford@gmail.com>
2018-03-24 08:17:58 -0600

    CGo whoami.

--- go-accel/accel.go
+++ go-accel/accel.go
@@ -1,26 +1,17 @@
 package main
 
-// #cgo LDFLAGS: -lm -lwiringPi
-// #include <stdio.h>
-// #include <errno.h>
-// #include <wiringPiI2C.h>
-import (
-	"C"
-	"fmt"
-)
+/*
+#cgo LDFLAGS: -lm -lwiringPi
+#include <errno.h>
+#include <wiringPiI2C.h>
+*/
+import "C"
+import "fmt"
 
 const I2C_ADDR = 0x6B
 
-// Initializes the accelerometer, returning the file descriptor
-func init() C.int {
-	var fd = C.wiringPiI2cSetup(I2C_ADDR)
-	
+func main() {
+	var fd := C.wiringPiI2CSetup(I2C_ADDR)
 	var whoami = int(C.wiringPiI2CReadReg8(fd))
 	fmt.Printf("Who am I: %d", whoami)
-
-	return fd
-}
-
-func main() {
-	init()
 }
