42c4b24a87a3a8db90e4666046aedc821bb3d389
John Hungerford <jhungerford@gmail.com>
2018-03-24 14:26:43 +0000

    Extract setup function.

--- go-accel/accel.go
+++ go-accel/accel.go
@@ -11,8 +11,14 @@ import "fmt"
 const I2C_ADDR = 0x6B
 const REG_WHO_AM_I = 0x0F
 
-func main() {
+func setup() C.int {
 	var fd = C.wiringPiI2CSetup(I2C_ADDR)
 	var whoami = int(C.wiringPiI2CReadReg8(fd, REG_WHO_AM_I))
 	fmt.Printf("Who am I: 0x%X\n", whoami)
+
+	return fd
+}
+
+func main() {
+	setup()
 }
