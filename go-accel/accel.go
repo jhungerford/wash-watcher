1eca093a501fd87b620ab44e69eda7d899dd53d1
John Hungerford <jhungerford@gmail.com>
2018-03-25 09:44:36 -0600

    Read an axis in C.

--- go-accel/accel.go
+++ go-accel/accel.go
@@ -4,6 +4,15 @@ package main
 #cgo LDFLAGS: -lm -lwiringPi
 #include <errno.h>
 #include <wiringPiI2C.h>
+
+// Reads accelerometer data out of two registers and returns a single uint
+// with 16 bits of data for one axis.
+uint readAxis(int fd, int low_addr, int high_addr) {
+	uint low = wiringPiI2CReadReg8(fd, low_addr)
+	uint high = wiringPiI2CReadReg8(fd, high_addr)
+
+	return (high & 0xFF) << 8 | (low & 0xFF)
+}
 */
 import "C"
 import "fmt"
@@ -40,13 +49,13 @@ type reg_axis struct {
 	low, high int
 }
 
-const AXIS_X = reg_axis{REG_OUTX_L_XL, REG_OUTX_H_XL}
-const AXIS_Y = reg_axis{REG_OUTY_L_XL, REG_OUTY_H_XL}
-const AXIS_Z = reg_axis{REG_OUTZ_L_XL, REG_OUTZ_H_XL}
+var AXIS_X = reg_axis{REG_OUTX_L_XL, REG_OUTX_H_XL}
+var AXIS_Y = reg_axis{REG_OUTY_L_XL, REG_OUTY_H_XL}
+var AXIS_Z = reg_axis{REG_OUTZ_L_XL, REG_OUTZ_H_XL}
 
 type accel_reading struct {
-	time Time
-	x, y, z int
+	time time.Time
+	x, y, z uint
 }
 
 // Connect to the LSM6DS3 module, verify the connection, and return the file descriptor.
@@ -55,9 +64,9 @@ type accel_reading struct {
 // Returns the file descriptor, or an error if the module could not be set up.
 func setup() (C.int, error) {
 	// Initialize WiringPi
-	var fd, errno = C.wiringPiI2CSetup(I2C_ADDR)
+	fd, err := C.wiringPiI2CSetup(I2C_ADDR)
 	if (fd < 0) {
-		return nil, fmt.Errorf("Error setting up i2c for address 0x%X - errno %d", I2C_ADDR, errno)
+		return 0, fmt.Errorf("Error setting up i2c for address 0x%X - %s", I2C_ADDR, err)
 	}
 
 	// Enable the accelerometer
@@ -68,35 +77,33 @@ func setup() (C.int, error) {
 	// Verify the whoami register to ensure everything is working
 	var whoami = int(C.wiringPiI2CReadReg8(fd, REG_WHOAMI))
 	if (whoami != VALUE_WHOAMI) {
-		return nil, fmt.Errorf("Wrong value (0x%X) for the whoami register.", whoami)
+		return 0, fmt.Errorf("Wrong value (0x%X) for the whoami register.", whoami)
 	}
 
 	return fd, nil
 }
 
-func readAxis(fd int, axis reg_axis) int {
-	var low = C.wiringPiI2CReadReg8(fd, reg_axis.low)
-	var high = C.wiringPiI2CReadReg8(fd, reg_axis.high)
-
-	return (high & 0xFF) << 8 | low
-}
-
-func readAccel(fd int) accel_reading {
+func readAccel(fd C.int) accel_reading {
 	return accel_reading{
 		time.Now(),
-		readAxis(fd, AXIS_X),
-		readAxis(fd, AXIS_Y),
-		readAxis(fd, AXIS_Z),
+		uint(C.readAxis(fd, AXIS_X.low, AXIS_X.high)),
+		uint(C.readAxis(fd, AXIS_Y.low, AXIS_Y.high)),
+		uint(C.readAxis(fd, AXIS_Z.low, AXIS_Z.high)),
 	}
 }
 
-func isAccelReady(fd int) bool {
-	var status = wiringPiI2CReadReg8(fd, REG_STATUS)
+func isAccelReady(fd C.int) bool {
+	var status = C.wiringPiI2CReadReg8(fd, REG_STATUS)
 	return (status & MASK_STATUS_XLDA) != 0
 }
 
 func main() {
-	fd = setup()
+	fd, err = setup()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 
 	for {
 		if isAccelReady(fd) {
