fd30680d11f4e712acd3d54bab24dc02f9219287
John Hungerford <jhungerford@gmail.com>
2018-04-14 15:37:51 -0600

    Show a message when laundry is running or done.

--- web/wash-watcher.js
+++ web/wash-watcher.js
@@ -1,10 +1,40 @@
 setInterval(checkLaundryStatus, 5000);
+let done = true;
+
+function isDone(variance) {
+  return variance > 1000000;
+}
+
+function setDone() {
+  document.getElementById("status").innerHTML = "Done!";
+  document.getElementById("body").classList.add("done");
+  document.getElementById("body").classList.remove("running");
+}
+
+function setRunning() {
+  document.getElementById("status").innerHTML = "Running...";
+  document.getElementById("body").classList.add("running");
+  document.getElementById("body").classList.remove("done");
+}
 
 function checkLaundryStatus() {
-  var request = new XMLHttpRequest();
+  const request = new XMLHttpRequest();
   request.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("status").innerHTML = this.responseText;
+    if (this.readyState === 4 && this.status === 200) {
+      const variance = Number(this.responseText);
+      const newDone = isDone(variance);
+
+      // Only update the page if the status changed
+      if (newDone !== done) {
+        done = newDone;
+        if (newDone) {
+          // Switched to running
+          setDone();
+        } else {
+          // Switched to done
+          setRunning();
+        }
+      }
     }
   };
   request.open("GET", "/variance", true);
